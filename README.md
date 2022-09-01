35d1a347e4b0665f7152f47ae9de9be573ea0929
wtto <wtto00@outlook.com>
2022-09-02 00:04:30 +0800

    - replace markdown to showdown - use github theme markdown render - add github markdown css to lib

--- README.md
+++ README.md
@@ -1,10 +1,14 @@
 # 临时 CDN
 
----
-
 ## video
 
 非官方库文件
 
 - https://wtto00.github.io/cdn/lib/video-flv/flv.min.js
 - https://wtto00.github.io/cdn/lib/video-flv/videojs-flvjs.min.js
+
+## github-markdown-css
+
+- https://wtto00.github.io/cdn/lib/github-markdown-css/github-markdown.css
+- https://wtto00.github.io/cdn/lib/github-markdown-css/github-markdown-dark.css
+- https://wtto00.github.io/cdn/lib/github-markdown-css/github-markdown-light.css
--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 import ejs from 'ejs';
-import { markdown } from 'markdown';
 import fs from 'fs';
+import showdown from 'showdown';
 import { minify } from 'html-minifier';
 
+const converter = new showdown.Converter();
+converter.setFlavor('github');
+
 const mdContent = fs.readFileSync('./README.md', { encoding: 'utf8' });
-const mdHtml = markdown.toHTML(mdContent);
+const mdHtml = converter.makeHtml(mdContent);
 
 const template = fs.readFileSync('./template.ejs', { encoding: 'utf8' });
 const htmlContent = ejs.render(template, { content: mdHtml });
 
-const miniHtml = minify(htmlContent, { collapseWhitespace: true });
+const miniHtml = minify(htmlContent, { collapseWhitespace: true, minifyCSS: true });
 
 fs.writeFileSync('./public/index.html', miniHtml);
