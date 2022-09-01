8ff7798795d20615bb26bedde70c71c5b8aac50c
wtto <wtto00@outlook.com>
2022-09-02 01:20:58 +0800

    github markdown

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
 import ejs from 'ejs';
 import fs from 'fs';
-import showdown from 'showdown';
+import { Octokit } from '@octokit/core';
 import { minify } from 'html-minifier';
 
-const converter = new showdown.Converter();
-converter.setFlavor('github');
-
-const mdContent = fs.readFileSync('./README.md', { encoding: 'utf8' });
-const mdHtml = converter.makeHtml(mdContent);
+const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN });
 
 const template = fs.readFileSync('./template.ejs', { encoding: 'utf8' });
-const htmlContent = ejs.render(template, { content: mdHtml });
+const mdContent = fs.readFileSync('./README.md', { encoding: 'utf8' });
 
-const miniHtml = minify(htmlContent, { collapseWhitespace: true, minifyCSS: true });
+const res = await octokit.request('POST /markdown', {
+  headers: {
+    accept: 'application/vnd.github+json',
+  },
+  text: mdContent,
+  mode: 'gfm',
+  context: 'wtto00/cdn',
+});
 
-fs.writeFileSync('./public/index.html', miniHtml);
+console.log(res);
+
+if (res.status === 200) {
+  const mdHtml = res.data;
+
+  const htmlContent = ejs.render(template, { content: mdHtml });
+
+  const miniHtml = minify(htmlContent, { collapseWhitespace: true, minifyCSS: true });
+
+  fs.writeFileSync('./public/index.html', miniHtml);
+}
